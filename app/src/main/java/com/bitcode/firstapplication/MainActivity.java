51c1016276fd1c53daee2ce64a7f03ecaeb40c6d
aishwarya <ashp17star@gmail.com>
2024-09-11 15:49:50 +0530

    Attaching Listeners

--- app/src/main/java/com/bitcode/firstapplication/MainActivity.java
+++ app/src/main/java/com/bitcode/firstapplication/MainActivity.java
@@ -2,27 +2,32 @@ package com.bitcode.firstapplication;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayoutCompat linearLayoutCompat;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button btnLogin;
 
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayoutCompat linearLayoutCompat = new LinearLayoutCompat(this);
+        linearLayoutCompat = new LinearLayoutCompat(this);
         linearLayoutCompat.setPadding(10,10,10,10);
         linearLayoutCompat.setOrientation(LinearLayoutCompat.VERTICAL);
         linearLayoutCompat.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
@@ -46,28 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 ViewGroup.LayoutParams.WRAP_CONTENT
         );
 
-        TextView usernameTextView = new TextView(this);
-        usernameTextView.setPadding(10,10,10,10);
-        usernameTextView.setHint("Enter Username");
-        usernameTextView.setBackgroundColor(R.color.black);
-        usernameTextView.setTextColor(R.color.white);
-        usernameTextView.setLayoutParams(layoutParams1);
+        usernameEditText = new EditText(this);
+        usernameEditText.setPadding(10,10,10,10);
+        usernameEditText.setHint("Enter Username");
+        usernameEditText.setBackgroundColor(R.color.black);
+        usernameEditText.setTextColor(R.color.white);
+        usernameEditText.setLayoutParams(layoutParams1);
 
-        linearLayoutCompat.addView(usernameTextView);
+        linearLayoutCompat.addView(usernameEditText);
 //        mainContainer.addView(usernameTextView);
 
-        TextView passwordTextView = new TextView(this);
-        passwordTextView.setHint("Enter Password");
-        passwordTextView.setPadding(10,10,10,10);
-        passwordTextView.setBackgroundColor(R.color.black);
-        passwordTextView.setTextColor(R.color.white);
-        passwordTextView.setLayoutParams(layoutParams1);
+        passwordEditText = new EditText(this);
+        passwordEditText.setHint("Enter Password");
+        passwordEditText.setPadding(10,10,10,10);
+        passwordEditText.setBackgroundColor(R.color.black);
+        passwordEditText.setTextColor(R.color.white);
+        passwordEditText.setLayoutParams(layoutParams1);
 //        passwordTextView.setBackgroundColor(R.color.);
 
-        linearLayoutCompat.addView(passwordTextView);
+        linearLayoutCompat.addView(passwordEditText);
 //        mainContainer.addView(passwordTextView);
 
-        Button btnLogin = new Button(this);
+        btnLogin = new Button(this);
         btnLogin.setText("Login");
         btnLogin.setPadding(10,10,10,10);
         btnLogin.setLayoutParams(layoutParams1);
@@ -76,7 +81,25 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutCompat.addView(btnLogin);
 //        mainContainer.addView(btnLogin);
 
+        btnLogin.setOnClickListener(new BtnLoginClickListener());
+
 //        setContentView(mainContainer);
         setContentView(linearLayoutCompat);
+    }
+
+    class BtnLoginClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            if (view == btnLogin){
+                if((usernameEditText.getText().toString().equals("bitcode") &&
+                        (passwordEditText.getText().toString().equals("bitcode@123")))){
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+                    Log.e("tag", "Success");
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Falied", Toast.LENGTH_SHORT).show();
+                    Log.e("tag", "Failure");
+                }
+            }
+        }
     }
 }
